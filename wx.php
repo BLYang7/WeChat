a417641065e6766520a65332c88dbf0ef22a6669
blyang <clear_d@163.com>
2016-04-19 13:39:49 +0800

    1

--- wx.php
+++ wx.php
@@ -177,35 +177,33 @@ class wechatCallbackapiTest
     //接收文本消息
     private function receiveText($object)
     {
-        $content = array();
-            $content[] = array("Title"=>"单图文标题",  "Description"=>"单图文内容", "PicUrl"=>"http://discuz.comli.com/weixin/weather/icon/cartoon.jpg", "Url" =>"http://m.cnblogs.com/?u=txw1958");
-            
-        // $keyword = trim($object->Content);
-        // //多客服人工回复模式
-        // if (strstr($keyword, "请问在吗") || strstr($keyword, "在线客服")){
-        //     $result = $this->transmitService($object);
-        //     return $result;
-        // }
 
-        //自动回复模式
-        // if (strstr($keyword, "文本")){
-        //     $content = "这是个文本消息";
-        // }else if (strstr($keyword, "表情")){
-        //     $content = "中国：".$this->bytes_to_emoji(0x1F1E8).$this->bytes_to_emoji(0x1F1F3)."\n仙人掌：".$this->bytes_to_emoji(0x1F335);
-        // }else if (strstr($keyword, "单图文")){
-        //     $content = array();
-        //     $content[] = array("Title"=>"单图文标题",  "Description"=>"单图文内容", "PicUrl"=>"http://discuz.comli.com/weixin/weather/icon/cartoon.jpg", "Url" =>"http://m.cnblogs.com/?u=txw1958");
-        // }else if (strstr($keyword, "图文") || strstr($keyword, "多图文")){
-        //     $content = array();
-        //     $content[] = array("Title"=>"多图文1标题", "Description"=>"", "PicUrl"=>"http://discuz.comli.com/weixin/weather/icon/cartoon.jpg", "Url" =>"http://m.cnblogs.com/?u=txw1958");
-        //     $content[] = array("Title"=>"多图文2标题", "Description"=>"", "PicUrl"=>"http://d.hiphotos.bdimg.com/wisegame/pic/item/f3529822720e0cf3ac9f1ada0846f21fbe09aaa3.jpg", "Url" =>"http://m.cnblogs.com/?u=txw1958");
-        //     $content[] = array("Title"=>"多图文3标题", "Description"=>"", "PicUrl"=>"http://g.hiphotos.bdimg.com/wisegame/pic/item/18cb0a46f21fbe090d338acc6a600c338644adfd.jpg", "Url" =>"http://m.cnblogs.com/?u=txw1958");
-        // }else if (strstr($keyword, "音乐")){
-        //     $content = array();
-        //     $content = array("Title"=>"最炫民族风", "Description"=>"歌手：凤凰传奇", "MusicUrl"=>"http://121.199.4.61/music/zxmzf.mp3", "HQMusicUrl"=>"http://121.199.4.61/music/zxmzf.mp3"); 
-        // }else{
-        //     $content = date("Y-m-d H:i:s",time())."\nOpenID：".$object->FromUserName."\n技术支持 方倍工作室";
-        // }
+        $keyword = trim($object->Content);
+        //多客服人工回复模式
+        if (strstr($keyword, "请问在吗") || strstr($keyword, "在线客服")){
+            $result = $this->transmitService($object);
+            return $result;
+        }
+
+        自动回复模式
+        if (strstr($keyword, "文本")){
+            $content = "这是个文本消息";
+        }else if (strstr($keyword, "表情")){
+            $content = "中国：".$this->bytes_to_emoji(0x1F1E8).$this->bytes_to_emoji(0x1F1F3)."\n仙人掌：".$this->bytes_to_emoji(0x1F335);
+        }else if (strstr($keyword, "单图文")){
+            $content = array();
+            $content[] = array("Title"=>"单图文标题",  "Description"=>"单图文内容", "PicUrl"=>"http://discuz.comli.com/weixin/weather/icon/cartoon.jpg", "Url" =>"http://m.cnblogs.com/?u=txw1958");
+        }else if (strstr($keyword, "图文") || strstr($keyword, "多图文")){
+            $content = array();
+            $content[] = array("Title"=>"多图文1标题", "Description"=>"", "PicUrl"=>"http://discuz.comli.com/weixin/weather/icon/cartoon.jpg", "Url" =>"http://m.cnblogs.com/?u=txw1958");
+            $content[] = array("Title"=>"多图文2标题", "Description"=>"", "PicUrl"=>"http://d.hiphotos.bdimg.com/wisegame/pic/item/f3529822720e0cf3ac9f1ada0846f21fbe09aaa3.jpg", "Url" =>"http://m.cnblogs.com/?u=txw1958");
+            $content[] = array("Title"=>"多图文3标题", "Description"=>"", "PicUrl"=>"http://g.hiphotos.bdimg.com/wisegame/pic/item/18cb0a46f21fbe090d338acc6a600c338644adfd.jpg", "Url" =>"http://m.cnblogs.com/?u=txw1958");
+        }else if (strstr($keyword, "音乐")){
+            $content = array();
+            $content = array("Title"=>"最炫民族风", "Description"=>"歌手：凤凰传奇", "MusicUrl"=>"http://121.199.4.61/music/zxmzf.mp3", "HQMusicUrl"=>"http://121.199.4.61/music/zxmzf.mp3"); 
+        }else{
+            $content = date("Y-m-d H:i:s",time())."\nOpenID：".$object->FromUserName."\n技术支持 方倍工作室";
+        }
 
         if(is_array($content)){
             if (isset($content[0])){
